4a3e3c584d9047a46a8878a24945e237e05adf7b
torian austin <torian03@outlook.com>
2022-11-23 23:23:55 -0800

    places the imgs in home.jsx and error404.jsx

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
       <main>
         <h1>404: Page Not Found</h1>
         <p>Oops, sorry, we can't find this page!</p>
+        <div>
+          <img id='not-found' src="/images/bunny.png" alt="bunny pic"/>
+          <div>
+            Photo by <a href="https://unsplash.com/@stuffinabox">Janan Lagerwall</a> on <a href="https://unsplash.com/s/photos/bunny">Unsplash</a>
+          </div>
+        </div>
       </main>
     </def>
   )
