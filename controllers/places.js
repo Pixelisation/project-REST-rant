ea1cef20415d4b509f0479f39c169275e29e9104
torian austin <torian03@outlook.com>
2022-11-27 11:27:52 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,32 +1,27 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+  res.render('places/index', {places})
+})
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('./places')
+})
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
-})
-
-router.get('/', (req, res) => {
-  let places = [{
-    name: 'buffalo wild wing',
-    city: 'Atlanta',
-    state: 'GA',
-    cuisines: 'Wings',
-    pic: 'images/Buffalo-wild-wings.png'
-  }, {
-    name: 'Robertos Taco Shop',
-    city: 'North Las Vegas',
-    state: 'NV',
-    cuisines: 'Taco',
-    pic: 'images/Robertos-Taco-Shop.png'
-  }]
-  
-  res.render('places/index', {places})
-})
-
-
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+  name: 'buffalo wild wing',
+  city: 'Atlanta',
+  state: 'GA',
+  cuisines: 'Wings',
+  pic: 'images/Buffalo-wild-wings.png'
+}, {
+  name: 'Robertos Taco Shop',
+  city: 'North Las Vegas',
+  state: 'NV',
+  cuisines: 'Taco',
+  pic: 'images/Robertos-Taco-Shop.png'
+}]
