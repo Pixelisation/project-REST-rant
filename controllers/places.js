901d1e2e1fde4c1728ed7b384919496243fffd3b
torian austin <torian03@outlook.com>
2022-11-30 17:56:52 -0800

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -37,6 +37,21 @@ router.get('/:id', (req, res) => {
   }
 })
 
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -60,7 +75,7 @@ router.get('/:id/edit', (req, res) => {
       res.render('error404')
   }
   else {
-    res.render('places/edit', { place: places[id] })
+    res.render('places/edit', { place: places[id],id })
   }
 })
 
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -2,16 +2,18 @@ const React = require("react");
 const Def = require("../default.jsx");
 
 function edit_form (data) {
+
+    console.log(data)
   return (
       <Def>
         <main>
           <h1>Edit Place</h1>
-          <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-              <div className="row">
-                  <div className="form-group col-sm-6">
-                      <label htmlFor="name">Place Name</label>
-                      <input className="form-control" id="name" name="name" value={data.place.name} required />
-                  </div>
+          <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <div className="row">
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="name">Place Name</label>
+                        <input className="form-control" id="name" name="name" value={data.place.name} required />
+                    </div>
                   <div className="form-group col-sm-6">
                       <label htmlFor="pic">Place Picture</label>
                       <input className="form-control" id="pic" name="pic" value={data.place.pic} />
