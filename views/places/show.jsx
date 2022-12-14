84b6c314288fb23494c8bbdb5f2abb2dfca49236
torian austin <torian03@outlook.com>
2022-12-14 02:08:44 -0800

    comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -23,21 +23,66 @@ function show(data) {
   }
   return (
     <Def>
-      <main>
-        <h1>{data.place.name}</h1>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
-        <hr/>
-        <h2>Comments</h2>
-        {comments}
-      </main>
+        <main>
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className="col-sm-6">
+                <h1>{ data.place.name }</h1>
+                <br />
+                <h2>
+                  Description
+                </h2>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+                <br />
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+                  Edit
+                </a>{` `}
+                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+                  <button type="submit" className="btn btn-danger">
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            <div className="row">
+              {comments}
+            </div>
+            <hr />
+            <h2>Got Your Own Rant or Rave?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="customRange2">Star Rating</label>
+                  <input type="range" name="range" min="0" max="5.5" 
+                  className="form-range" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label className="form-check-label" htmlFor="flexCheckDefault">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control form-check-input"/>
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>
+          </main>
     </Def>
   );
 }
