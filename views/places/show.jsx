d82c8016a94b084453778b1fe3c4f97ea9057961
torian austin <torian03@outlook.com>
2022-12-14 02:44:06 -0800

    fix the edit and delete button

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,25 @@ function show(data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated!
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) =>{
+      return tot + c.stars
+    },0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐️'
+    }
+    rating = (
+      <h3>
+        {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border">
@@ -33,6 +51,10 @@ function show(data) {
               </div>
               <div className="col-sm-6">
                 <h1>{ data.place.name }</h1>
+                <h2>
+                  rating
+                </h2>
+                {rating}
                 <br />
                 <h2>
                   Description
